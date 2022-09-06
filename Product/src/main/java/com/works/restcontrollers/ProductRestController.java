210e835c1c9bc244a3ff4a694b4aeed75b7c6883
Hakan ÖZER <hakanozer02@gmail.com>
2022-09-06 09:56:51 +0300

    Product Service

--- Product/src/main/java/com/works/restcontrollers/ProductRestController.java
+++ Product/src/main/java/com/works/restcontrollers/ProductRestController.java
@@ -1,13 +1,14 @@
 package com.works.restcontrollers;
 
+import com.works.entities.Product;
 import com.works.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/product")
 public class ProductRestController {
 
     final ProductService pService;
@@ -15,6 +16,16 @@ public class ProductRestController {
     @GetMapping("/apiKey")
     public ResponseEntity apiKey() {
         return pService.apiKey();
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity save(@RequestBody Product product) {
+        return pService.save(product);
+    }
+
+    @GetMapping("/findId/{pid}")
+    public ResponseEntity findId( @PathVariable int pid ) {
+        return pService.findId(pid);
     }
 
 }
--- Product/src/main/java/com/works/services/ProductService.java
+++ Product/src/main/java/com/works/services/ProductService.java
@@ -1,6 +1,8 @@
 package com.works.services;
 
+import com.works.entities.Product;
 import com.works.profile.ProfileConfig;
+import com.works.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RefreshScope
@@ -17,6 +20,7 @@ import java.util.Map;
 public class ProductService {
 
     final ProfileConfig config;
+    final ProductRepository pRepo;
 
     @Value("${data.apikey}")
     private String apikey;
@@ -25,6 +29,26 @@ public class ProductService {
         Map<String, Object> hm = new LinkedHashMap<>();
         hm.put("apikey", apikey);
         hm.put("config", config.config());
+        return new ResponseEntity(hm, HttpStatus.OK);
+    }
+
+    public ResponseEntity save(Product product) {
+        pRepo.save(product);
+        Map<String, Object> hm = new LinkedHashMap<>();
+        hm.put("status", true);
+        hm.put("result", product);
+        return new ResponseEntity(hm, HttpStatus.OK);
+    }
+
+    public ResponseEntity findId( int pid ) {
+        Optional<Product> optionalProduct = pRepo.findById((long) pid);
+        Map<String, Object> hm = new LinkedHashMap<>();
+        if ( optionalProduct.isPresent() ) {
+            hm.put("status", true);
+            hm.put("result", optionalProduct.get());
+        }else {
+            hm.put("status", false);
+        }
         return new ResponseEntity(hm, HttpStatus.OK);
     }
 
